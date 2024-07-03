277fbdd0e560d55518c335f2984ffe38d8ed1a33
Luiz Hilário <70781613+D1nuh@users.noreply.github.com>
2024-07-03 00:24:03 -0300

    Atualizar o __init__.py

--- __init__.py
+++ __init__.py
@@ -5,10 +5,10 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def raiz_leitura():
+    return {"Teste_Message": "Mensagem de teste"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+def raiz_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
